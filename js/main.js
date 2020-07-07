82b97416e7f637b140db8c9c8be531f18879f5f3
Sota <sooota09@gmail.com>
2020-07-07 14:10:28 +0900

    [update] おみくじのランダム表示

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    btn.textContent = 'hit!';
+    const n = Math.floor(Math.random() * 3); // 0から2までの整数値をランダムに生成しnに代入
+    // btn.textContent = n;
+
+    switch (n) {
+      case 0:
+        btn.textContent = '大吉';
+        break;
+      case 1:
+        btn.textContent = '中吉';
+        break;
+      case 2:
+        btn.textContent = '凶';
+        break;
+    }
   });
 }
